042cb7f9e49a2ce23f3e2297a9a7f122f80c7c45
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2019-11-10 23:13:34 +0000

    primeira tentativa de atualizar os jsons

--- fs_scrapper/app/__init__.py
+++ fs_scrapper/app/__init__.py
@@ -1,5 +1,24 @@
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import atexit
+import time
+from app import updater
 
 app = Flask(__name__)
 
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=updater.updater,
+    trigger=IntervalTrigger(seconds=2),
+    id='printing_time_job',
+    name='Print time every 2 seconds',
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
 from app import routes
+
+# run Flask app in debug mode
+app.run(debug=True)
